65381722725fba0dd789efc21d21759400a5ee67
Nathan Stewart <stewart.nathang@gmail.com>
2017-10-03 09:17:13 -0700

    style remove med button

--- src/components/Medication.jsx
+++ src/components/Medication.jsx
@@ -8,6 +8,11 @@ function Medication(props) {
     paddingLeft: "5px"
   }
 
+  var removeButtonStyle = {
+    float: "right",
+    marginRight: "5px"
+  }
+
   return(
     <div style={medStyle}>
       <h3>{props.name}</h3>
@@ -16,7 +21,11 @@ function Medication(props) {
       <h5>Doctors Orders:</h5>
       <p>{props.doctorsOrders}</p>
       <Button>Take Medicine</Button>
-      <Button onClick={() => {props.handleRemovingMedicine(props.id)}}>Discontinue Medicine</Button>
+      <Button
+        style={removeButtonStyle}
+        bsSize="small"
+        bsStyle="danger"
+        onClick={() => {props.handleRemovingMedicine(props.id)}}>Discontinue Medicine</Button>
       <hr/>
     </div>
   );
